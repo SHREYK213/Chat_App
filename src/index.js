32d663429a99fb3cfeb68be213f174f3182ab6d6
SHREYK213 <shreyaspoojary240@gmail.com>
2023-06-08 16:15:16 +0530

    Messages Getting sent and location available

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
 const path = require('path')
 const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../path')
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New Websocket Connection')
+
+    socket.emit('message', 'Welcome!!')
+    socket.broadcast.emit('message', 'A new user has Joined')
+
+    socket.on('sendMessage', message => {
+        io.emit('message', message)
+    })
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has Left')
+    })
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is up on ${3000} click: http://localhost:3000/`)
+})
