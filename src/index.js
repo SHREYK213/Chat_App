96646b89eeb7a8a47744a0671349cadbce20732d
shreyas <shreyaspoojary240@gmail.com>
2023-09-15 00:51:18 +0530

    Rooms and styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
-
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
@@ -15,22 +15,31 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.on('join', ({ username, room }) => {
+        socket.join(room)
+
+        socket.emit('message', generateMessage('Welcome!'))
+        socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`))
+
+        // socket.emit - specific client
+        // io.emit -every connected client
+        // socket.broadcast.client - send to every client except sender
+        // io.to.emit, socket.broadcast.to.emit - within a specific chatroom
+    })
 
     socket.on('sendMessage', (message, callback) => {
 
-        io.emit('message', message)
+        io.to('india').emit('message', generateMessage(message))
         callback()
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
